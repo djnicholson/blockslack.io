40cb74ecd40d280f1bda434f1f7e0ea80ed00630
David Nicholson <me@djnicholson.com>
2019-01-09 19:51:48 +0000

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
-mkdir server
+mkdir server 2>/dev/null
 git pull
 find . ! -path '*/\.*' ! -name 'CNAME' ! -name 'deploy.sh' -type f -exec rm -f {} +
 cp ~/blockslack/deploy/* .
