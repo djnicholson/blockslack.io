91c1b7bbf97fb31d55ca8943b8b1664d8ef16281
David Nicholson <me@djnicholson.com>
2019-01-09 19:49:53 +0000

    deploy server bits

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
+mkdir server
 git pull
 find . ! -path '*/\.*' ! -name 'CNAME' ! -name 'deploy.sh' -type f -exec rm -f {} +
 cp ~/blockslack/deploy/* .
+cp ~/blockslack/server/* server/
 git add -u :/
 git add *
 git commit -a -m "Deployment"
